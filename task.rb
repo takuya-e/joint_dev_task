b85fd26368dc0fc29160113328b4c11ebbf0109e
takuya_endu <takuya.e.0702@gmail.com>
2020-11-25 11:50:52 +0900

    ruby Q19_Q20

--- task.rb
+++ task.rb
@@ -179,7 +179,7 @@ end
 
 class UserQ18
   # 以下に回答を記載
-    def initialize(params)
+    def initialize(**params)
     @name = params[:name]
     @age = params[:age]
   end
@@ -219,12 +219,36 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
+  attr_reader :name, :age
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+  attr_reader :name
+  attr_accessor :entry_fee
 
+  def initialize(**params)
+    @name = params[:name]
+    @entry_fee = params[:entry_fee]
+  end
+
+  def info_entry_fee(user)
+    judge_fee = case user.age
+    when 0..5
+      @entry_fee[:infant]
+    when 6..12
+      @entry_fee[:children]
+    when 13..64
+      @entry_fee[:adult]
+    when 65..120
+      @entry_fee[:senior]
+    end
+    puts "#{user.name}さんの入場料金は#{judge_fee}円です"
+  end
 end
 
 
